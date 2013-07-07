40551577db63b63816474cf899a7b3c363a7d7de
Josh Reed <j.reed@realiefcenters.com>
2013-07-07 15:56:41 -0500

    Add assets plugin

---
+++ plugins/assets.groovy
@@ -0,0 +1,23 @@
+class AssetsPlugin {
+
+	def afterIndex(config, index) {
+		def ant = new AntBuilder()
+
+		// walk through our paths and copy over assets
+		config?.paths?.assets?.each { path ->
+			def global = new File(path)
+			if (global.exists()) {
+				ant.copy(todir: new File(config.destination, path).absolutePath) {
+					fileset(dir: global.absolutePath)
+				}
+			}
+
+			def local = new File(config.source, path)
+			if (local.exists()) {
+				ant.copy(todir: new File(config.destination, path).absolutePath) {
+					fileset(dir: local.absolutePath)
+				}
+			}
+		}
+	}
+}
