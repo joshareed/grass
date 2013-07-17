37371b040e476011eaa789649f7186047e3d5fd0
Josh Reed <j.reed@realiefcenters.com>
2013-07-17 03:09:03 -0500

    Set the page summary to the full content by default

--- plugins/markdown.groovy
+++ plugins/markdown.groovy
@@ -7,6 +7,7 @@ class MarkdownPlugin {
 		if (page?.path?.name?.endsWith('.md') || page?.path?.name?.endsWith('.markdown')) {
 			def processor = new MarkdownProcessor()
 			page.content = processor.markdown(page.content)
+			page.summary = page.content
 		}
 	}
 }
