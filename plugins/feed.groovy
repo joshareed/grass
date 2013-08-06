75ea7b83447b15b469254ce41ce0d824c4c12067
Josh Reed <j.reed@realiefcenters.com>
2013-08-05 20:53:29 -0500

    Generate separate feeds for each tag

--- plugins/feed.groovy
+++ plugins/feed.groovy
@@ -2,16 +2,34 @@ class FeedPlugin {
 	def LINKS_TAG = /<feed:links\/>/
 
 	def config
+	def feeds = [:]
 
 	def afterIndex(index, pages) {
 		def posts = pages.findAll { it.post }.sort { a, b -> b.date <=> a.date }
 		def content = applyTemplate('feed/atom.xml', '', newBinding(posts: posts, site: config.site))
 		addPage(content: content, name: 'Atom Feed', title: 'Atom Feed', date: new Date(), out: 'atom.xml')
+
+		if (config.tags) {
+			generateTagFeeds()
+		}
+	}
+
+	private generateTagFeeds() {
+		config.tags.each { k, v ->
+			def tag = normalize(k)
+			def posts = v.sort { a, b -> b.date <=> a.date }
+			def content = applyTemplate('feed/atom.xml', '', newBinding(posts: posts, site: config.site))
+			addPage(content: content, name: "${k} Feed", title: "${k} Feed", date: new Date(), out: "feeds/${tag}.xml")
+		}
 	}
 
 	def beforeWrite(page) {
 		if (page.content.contains(LINKS_TAG)) {
 			page.content = page.content.replace(LINKS_TAG, applyTemplate('feed/links', '', newBinding([:])).toString())
 		}
+	}
+
+	private normalize(tag) {
+		URLEncoder.encode(tag.toLowerCase().replace(' ', '-'), "UTF-8")
 	}
 }
